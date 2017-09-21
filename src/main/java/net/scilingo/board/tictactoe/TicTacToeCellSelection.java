53c46924b9cfe42d9c138950158a2f64facd4840
Anthony Scilingo <ajscilingo@gmail.com>
2017-09-21 01:12:14 -0500

    Fixed Computer Player Bugs
    Fixed Issue where Computer Player could no longer guess next move

--- src/main/java/net/scilingo/board/tictactoe/TicTacToeCellSelection.java
+++ src/main/java/net/scilingo/board/tictactoe/TicTacToeCellSelection.java
@@ -1,8 +1,10 @@
 package net.scilingo.board.tictactoe;
 
+import java.util.Comparator;
+
 import net.scilingo.board.CellSelection;
 
-public enum TicTacToeCellSelection implements CellSelection {
+public enum TicTacToeCellSelection implements CellSelection{
 	UPPER_LEFT,
 	UPPER_MIDDLE,
 	UPPER_RIGHT,
@@ -23,13 +25,13 @@ public enum TicTacToeCellSelection implements CellSelection {
 			case UPPER_RIGHT:
 				return new TicTacToeCellSelection[] {UPPER_MIDDLE, MIDDLE_MIDDLE, MIDDLE_RIGHT};
 			case MIDDLE_LEFT:
-				return new TicTacToeCellSelection[] {UPPER_RIGHT, UPPER_MIDDLE, MIDDLE_MIDDLE, LOWER_LEFT, LOWER_MIDDLE};
+				return new TicTacToeCellSelection[] {UPPER_LEFT, UPPER_MIDDLE, MIDDLE_MIDDLE, LOWER_LEFT, LOWER_MIDDLE};
 			case MIDDLE_MIDDLE:
 				return new TicTacToeCellSelection[] {UPPER_RIGHT, UPPER_MIDDLE, UPPER_LEFT, MIDDLE_RIGHT, MIDDLE_LEFT, LOWER_RIGHT, LOWER_MIDDLE, LOWER_LEFT};
 			case MIDDLE_RIGHT:
 				return new TicTacToeCellSelection[]{UPPER_RIGHT, UPPER_MIDDLE, MIDDLE_MIDDLE, LOWER_RIGHT, LOWER_MIDDLE};
 			case LOWER_LEFT:
-				return new TicTacToeCellSelection[]{MIDDLE_RIGHT, MIDDLE_MIDDLE, LOWER_MIDDLE};
+				return new TicTacToeCellSelection[]{MIDDLE_LEFT, MIDDLE_MIDDLE, LOWER_MIDDLE};
 			case LOWER_MIDDLE:
 				return new TicTacToeCellSelection[]{MIDDLE_LEFT, MIDDLE_MIDDLE, MIDDLE_RIGHT, LOWER_LEFT, LOWER_RIGHT};
 			case LOWER_RIGHT:
--- src/main/java/net/scilingo/board/tictactoe/TicTacToePlayerComputer.java
+++ src/main/java/net/scilingo/board/tictactoe/TicTacToePlayerComputer.java
@@ -3,6 +3,10 @@ package net.scilingo.board.tictactoe;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.Stack;
 import net.scilingo.board.CellSelection;
 
@@ -13,31 +17,82 @@ public class TicTacToePlayerComputer extends TicTacToePlayerTwo {
 	// Last Recorded move made by Player1
 	private TicTacToeCellSelection _playerOnesLastMove;
 	// List Of Optional Moves For Computer Player
-	private Stack<CellSelection> _availableMoves = new Stack<CellSelection>();
+	private Stack<CellSelection> _preferedMoves = new Stack<CellSelection>();
+	private Set<CellSelection> _availableMoves = new HashSet<CellSelection>();
 	
 	TicTacToePlayerComputer() {
 		super();
+		resetAvailableMoves();
 	}
 	
 	public CellSelection getNextMove() {
-		if(!this._availableMoves.isEmpty())
-			return this._availableMoves.pop();
-		else
-			return null;
+		
+		CellSelection myNextPotentialMove = null;
+		
+		if(!this._preferedMoves.isEmpty()) {
+			
+			myNextPotentialMove = 	_preferedMoves.pop();
+			
+			while(!_availableMoves.contains(myNextPotentialMove)) {
+				if(!_preferedMoves.isEmpty()) {
+					myNextPotentialMove = _preferedMoves.pop();
+				}
+				else {
+					myNextPotentialMove = pickFromAvailableCells();
+				}
+			}
+		}
+		else {
+			myNextPotentialMove = pickFromAvailableCells();
+		}
+		
+		// remove myNextPotentialMove from _availableMoves
+		_availableMoves.remove(myNextPotentialMove);		
+		return myNextPotentialMove;
 	}
 	
 	void recordPlayerOnesMove(CellSelection cellSelection) {
 		// record player1's last move
 		this._playerOnesLastMove = (TicTacToeCellSelection)cellSelection;
 		
+		// Remove Player 1's cell selection from list of available moves
+		_availableMoves.remove(cellSelection);
+		
 		// clear available moves if not empty
-		if(!this._availableMoves.isEmpty())
-			this._availableMoves.clear();
+		if(!this._preferedMoves.isEmpty())
+			this._preferedMoves.clear();
 		
 		// setup a list of available strategic moves
-		this._availableMoves.addAll(Arrays.asList(TicTacToeCellSelection.getAdjacentCells(this._playerOnesLastMove)));
+		this._preferedMoves.addAll(Arrays.asList(TicTacToeCellSelection.getAdjacentCells(this._playerOnesLastMove)));
 		
 		// shuffle possible moves
-		Collections.shuffle(this._availableMoves);
-	}	
+		Collections.shuffle(this._preferedMoves);
+	}
+	
+	@Override
+	protected void reset() {
+		super.reset();
+		resetAvailableMoves();
+	}
+	
+	private void resetAvailableMoves() {
+		_availableMoves.add(TicTacToeCellSelection.UPPER_LEFT);
+		_availableMoves.add(TicTacToeCellSelection.UPPER_MIDDLE);
+		_availableMoves.add(TicTacToeCellSelection.UPPER_RIGHT);
+		_availableMoves.add(TicTacToeCellSelection.MIDDLE_LEFT);
+		_availableMoves.add(TicTacToeCellSelection.MIDDLE_MIDDLE);
+		_availableMoves.add(TicTacToeCellSelection.MIDDLE_RIGHT);
+		_availableMoves.add(TicTacToeCellSelection.LOWER_LEFT);
+		_availableMoves.add(TicTacToeCellSelection.LOWER_MIDDLE);
+		_availableMoves.add(TicTacToeCellSelection.LOWER_RIGHT);
+	}
+	
+	private CellSelection pickFromAvailableCells() {
+		Iterator<CellSelection> availableMovesItr = _availableMoves.iterator();
+		
+		if(availableMovesItr.hasNext())
+			return availableMovesItr.next();
+		else
+			return null;
+	}
 }
